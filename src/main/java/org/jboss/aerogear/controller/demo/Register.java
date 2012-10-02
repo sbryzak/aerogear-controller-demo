ee696d2cf18526d9ec494640c7491b29b1771d9b
Bruno Oliveira <bruno@abstractj.org>
2012-10-02 11:42:44 -0300

    Including registration process and refactoring to the latest abstraction

---
+++ src/main/java/org/jboss/aerogear/controller/demo/Register.java
@@ -0,0 +1,34 @@
+package org.jboss.aerogear.controller.demo;
+
+import org.jboss.aerogear.controller.demo.model.User;
+import org.jboss.aerogear.security.dsl.AuthenticationManager;
+import org.jboss.aerogear.security.dsl.IDMHelper;
+import org.jboss.aerogear.security.model.AeroGearUser;
+import org.jboss.picketlink.idm.model.SimpleUser;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+@Stateless
+public class Register {
+
+    @Inject
+    private IDMHelper idm;
+
+    @Inject
+    private AuthenticationManager authenticationManager;
+
+    public void index() {
+        System.out.println("Login page!");
+    }
+
+    public AeroGearUser register(User user) {
+
+        System.out.println("User: " + user.getId());
+//        idm.grant("admin").to(user);
+//
+//        boolean logged = authenticationManager.login(user.getUsername(), user.getPassword());
+//        System.out.println("Registered? " + logged);
+        return user;
+    }
+}
